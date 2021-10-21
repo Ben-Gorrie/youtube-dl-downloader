d3ca8af4aaa1e00932feb90deda58c746b8da97f
Ben-Gorrie <ben24601g@gmail.com>
2021-10-22 00:31:21 +0100

    Update yt_2_mp3.py

--- yt_2_mp3.py
+++ yt_2_mp3.py
@@ -4,9 +4,8 @@ import os
 #enter song urls in the list as strings
 songs = [""]
 commands = []
-os.system("mkdir ~/Songs")
 for i in songs:
-    string = "youtube-dl -o '~/Songs/%(title)s.%(ext)s' --extract-audio --audio-format mp3 " + i
+    string = "youtube-dl -o '~/Music/%(title)s.%(ext)s' --extract-audio --audio-format mp3 " + i
     commands.append(string)
     
 for i in commands:
